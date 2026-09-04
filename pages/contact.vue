b50341f0f6726dd53f47c59683068ab68fcd9907
tatyanatomsickova <116955953+tatyanatomsickova@users.noreply.github.com>
2026-09-04 14:45:43 +0200

    Update contact.vue

--- pages/contact.vue
+++ pages/contact.vue
@@ -3,7 +3,7 @@
     id="fs-frm"
     name="message-form"
     accept-charset="utf-8"
-    action="https://formspree.io/f/xwpjgdrj"
+    action="https://formspree.io/f/t.tomsickova@seznam.cz"
     method="post"
     class="max-w-md mx-auto"
   >
